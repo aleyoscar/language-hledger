2779792d4c96262243b77fa5afb17b4336bde253
aleyoscar <mail@aleyoscar.com>
2026-04-14 09:06:55 -0500

    package skeleton

---
+++ spec/language-hledger-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageHledgerView from '../lib/language-hledger-view';
+
+describe('LanguageHledgerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
