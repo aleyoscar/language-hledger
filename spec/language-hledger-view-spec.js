8e78113a7585ad89420d91ddd0e06df7d8c67f55
aleyoscar <mail@aleyoscar.com>
2026-04-14 10:29:12 -0500

    refactor(core): Remove obsolete files and folders from skeleton

--- spec/language-hledger-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageHledgerView from '../lib/language-hledger-view';
-
-describe('LanguageHledgerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
